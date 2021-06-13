c52ba63401b57eb91c6296aa507e62c5f61a2d1c
BhavaniSingam <39860323+BhavaniSingam@users.noreply.github.com>
2021-06-13 11:28:24 +0530

    Create testBMSData.py

---
+++ Sender/testBMSData.py
@@ -0,0 +1,13 @@
+import unittest
+import GenerateBMSData
+class test_streaming_bms_data(unittest.TestCase):
+    def test_format_battery_parameter(self):
+        bms_param = GenerateBMSData.generateBatteryParameters()
+        self.assertTrue(bms_param['temperature'])
+        self.assertTrue(bms_param['soc'])
+    def test_stream_bms_reading_send(self):
+        GenerateBMSData.stream_readings(6)
+        
+       
+if __name__ == '__main__':
+    unittest.main()
