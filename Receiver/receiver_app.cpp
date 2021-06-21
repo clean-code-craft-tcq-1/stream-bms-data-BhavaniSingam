0aff5380f62a2120815f8eba9f2cb014211226db
selvam-manojkumar <77610809+selvam-manojkumar@users.noreply.github.com>
2021-06-21 15:47:04 +0530

    Update receiver_app.cpp

--- Receiver/receiver_app.cpp
+++ Receiver/receiver_app.cpp
@@ -6,44 +6,56 @@
 #include <sstream>
 #include "bms_receiver.h"
 using namespace std;
+bms_process_receiver recvObj;
+char firstdata = true;
+
+void update_minmax(BMS_DATA *sampleData)
+{
+	if (firstdata)
+	{
+		recvObj.init_minmax(sampleData);
+		firstdata = false;
+	}
+	else
+	{
+		recvObj.compute_minmaxSOC(sampleData);
+		recvObj.compute_minmaxTemp(sampleData);
+	}
+}
+
+void assignLength(int* movingLength)
+{
+	*movingLength = (*movingLength <= MOVING_LENGTH) ? *movingLength : MOVING_LENGTH;
+}
 
 int main()
 {
-	BMS_DATA sampleData = {0,0};
-	char firstdata = true;
+	BMS_DATA sampleData, prev_sample_data = {0,0};
 	std::stringstream InputData;
-	bms_process_receiver recvObj;
+	float MinSOC = 0, MaxSOC = 0, MinTemp = 0, MaxTemp = 0;
 	long avgCalcCount = 0;
 	double SOC_avg = 0,temp_avg = 0;
-	int position = 0;
+	int position = 0, movingLength = 0;
+	int newAvg = 0;
 
 	while (1)
 	{
 		avgCalcCount++;
+		movingLength = avgCalcCount;
 		InputData.clear();
 		string input;
 		getline(cin, input);
+		//Sleep(500);
 		InputData << input;
 		sampleData = recvObj.extractData(InputData);
-		
-		if(firstdata)
-		{
-			recvObj.init_minmax(&sampleData);
-		}
-		else
-		{
-			recvObj.compute_minmax(&sampleData);
-		}
-		if (avgCalcCount >= MOVING_LENGTH)
-		{
-			SOC_avg = recvObj.movingAvg(recvObj.SOCArray, &recvObj.SOC_sum, position, MOVING_LENGTH, sampleData.SOC);
-			temp_avg = recvObj.movingAvg(recvObj.tempArray, &recvObj.temp_sum, position, MOVING_LENGTH, sampleData.temperature);
-		}
+		update_minmax(&sampleData);
+		assignLength(&movingLength);
+
+		SOC_avg = recvObj.movingAvg(recvObj.SOCArray, &recvObj.SOC_sum, position, movingLength, sampleData.SOC);
+		temp_avg = recvObj.movingAvg(recvObj.tempArray, &recvObj.temp_sum, position, movingLength, sampleData.temperature);
 
 		position++;
-		if (position >= MOVING_LENGTH) {
-			position = 0;
-		}
+		position = (position >= MOVING_LENGTH) ? position : 0;
 
 		printf("MinSOC = %02.2f, MaxSOC = %02.2f, MinTemperature = %02.2f, Maxtemperature = %02.2f, AvgSOC = %02.2f, Avgtemp = %02.2f \n", \
 			recvObj.MinSOC, recvObj.MaxSOC, recvObj.MinTemp, recvObj.MaxTemp, SOC_avg, temp_avg);
