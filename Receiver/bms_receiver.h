c549fa9afc3ad6ed44164b5c27b1902e872dde0f
selvam-manojkumar <77610809+selvam-manojkumar@users.noreply.github.com>
2021-06-21 17:45:06 +0530

    Update bms_receiver.h

--- Receiver/bms_receiver.h
+++ Receiver/bms_receiver.h
@@ -16,9 +16,10 @@ public:
 	float SOCArray[MOVING_LENGTH] = { 0 };
 	float tempArray[MOVING_LENGTH] = { 0 };
 	BMS_DATA sampleData = {0, 0};
+	BMS_DATA bms_params;
 	float MinSOC, MaxSOC, MinTemp, MaxTemp;
 	double SOC_sum = 0, temp_sum = 0;
-	BMS_DATA extractData(std::stringstream& IPData);
+	BMS_DATA* extractData(std::stringstream& IPData);
 	void init_minmax(BMS_DATA *sampleData);
 	void compute_minmaxSOC(BMS_DATA *sampleData);
 	void compute_minmaxTemp(BMS_DATA *sampleData);
