594dd5d69886b1642579f68ca25962470d3c9eed
selvam-manojkumar <77610809+selvam-manojkumar@users.noreply.github.com>
2021-06-21 19:35:36 +0530

    Update test.cpp

--- Receiver/test/test.cpp
+++ Receiver/test/test.cpp
@@ -14,7 +14,7 @@ TEST_CASE("data reception test") {
   REQUIRE(validate_data(teststring));
   IPDATA <<  teststring;
   sampleData = testObj.extractData(IPData);
-  REQUIRE( sampleDat->.SOC == 59 );
+  REQUIRE( sampleDat->SOC == 59 );
   REQUIRE( sampleData->temperature == 15 );
   
 }
