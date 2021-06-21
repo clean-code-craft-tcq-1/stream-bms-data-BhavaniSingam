3f98c4c3fd49e13ac0c52ca0aa174ac7611ebb35
selvam-manojkumar <77610809+selvam-manojkumar@users.noreply.github.com>
2021-06-21 21:34:37 +0530

    Update testBMSData.py

--- Sender/testBMSData.py
+++ Sender/testBMSData.py
@@ -6,7 +6,7 @@ class test_streaming_bms_data(unittest.TestCase):
         self.assertTrue(bms_param['temperature'])
         self.assertTrue(bms_param['soc'])
     def test_stream_bms_reading_send(self):
-        GenerateBMSData.stream_readings(6)
+        GenerateBMSData.stream_readings(50)
         
        
 if __name__ == '__main__':
